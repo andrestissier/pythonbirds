9c48dadeea6ba57a00973196931af33f53bb3150
andrestissier <andrestissier@gmail.com>
2021-07-05 21:14:40 -0300

    Criado atributo de clase olhos

--- oo/pessoa.py
+++ oo/pessoa.py
@@ -1,5 +1,7 @@
 class Pessoa:
-    def __init__(self, *filhos, nome=None, idade=35):
+    olhos = 2 #atributo default ou atributo de classe, todos vão ter esse atributo
+                #atributo de classe
+    def __init__(self, *filhos, nome=None, idade=35): #atributo de instância
         self.idade = idade
         self.nome = nome
         self.filhos = list(filhos)
@@ -17,10 +19,17 @@ if __name__ == '__main__':
     print(luciano.idade)
     for filho in luciano.filhos:
         print(filho.nome)
-    luciano.sobrenome = 'Ramalho'
+    luciano.sobrenome = 'Ramalho' #atributo dinamicamente através de atribuição e removidos por del
     del luciano.filhos
     #removido apenas em luciano
+    luciano.olhos = 1 #agora ele irá passar a fazer parte do dander dict do luciano e será um objeto diferente
+    del luciano.olhos #apagando apenas do luciano e não da classe, irá receber o atributo de classe novamente
     #dander dict mostra todos os atributos
-    print(luciano.__dict__)
+    print(luciano.__dict__) #dander dict apresenta apenas atributos de instância e não de classe geral
     print(renzo.__dict__)
+    Pessoa.olhos = 3 #mudei o atributo da classe, mas o de luciano continua 1
+    print(Pessoa.olhos)
+    print(luciano.olhos)
+    print(renzo.olhos)
+    print(id(Pessoa.olhos), id(luciano.olhos))
 
